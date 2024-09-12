0162364d5b6609944f390870f6c83709a4d600de
Ilya Shablovsky <shablovsky.i@jetwhales.com>
2024-09-12 16:44:49 +0300

    Rework world id passing

--- Addons/Prototype/Players/Runtime/Utils/PlayerUtils.cs
+++ Addons/Prototype/Players/Runtime/Utils/PlayerUtils.cs
@@ -20,8 +20,8 @@ namespace ME.BECS.Players {
         public static void SetActivePlayer(in PlayerAspect playerAspect) => PlayerStatic.activePlayer.Data = playerAspect.ent;
 
         [INLINE(256)]
-        public static Ent CreatePlayer(in SystemContext context, uint index, in Ent team, JobInfo jobInfo = default) {
-            var ent = Ent.New(context);
+        public static Ent CreatePlayer(uint index, in Ent team, JobInfo jobInfo) {
+            var ent = Ent.New(jobInfo);
             var aspect = ent.GetOrCreateAspect<PlayerAspect>();
             aspect.index = index;
             aspect.ent.Get<PlayerComponent>().team = team;
