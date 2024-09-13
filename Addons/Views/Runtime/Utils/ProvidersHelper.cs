4ce3874eb6dc06a2ec86416a1fcb2f898f375afd
Alex Silaev <chrome.alex@gmail.com>
2024-09-13 12:14:09 +0300

    ProvidersHelper refactoring: use queue instead of recursive method call

--- Addons/Views/Runtime/Utils/ProvidersHelper.cs
+++ Addons/Views/Runtime/Utils/ProvidersHelper.cs
@@ -35,41 +35,67 @@ namespace ME.BECS.Views {
     
     public static class ProvidersHelper {
 
+        public struct TransformItem {
+
+            public UnityEngine.Transform obj;
+            public Ent parent;
+
+        }
+
         public static Ent ConstructEntFromPrefab(UnityEngine.Transform prefab, in Ent parentEnt, in World world) {
 
-            var ent = Ent.New(world);
-            var tr = ent.GetOrCreateAspect<TransformAspect>();
-            ent.SetParent(parentEnt);
-            tr.Set(prefab);
-            if (parentEnt.IsAlive() == true) {
-                tr.worldMatrix = math.mul(prefab.localToWorldMatrix, prefab.root.localToWorldMatrix.inverse);
-            } else {
-                tr.worldMatrix = float4x4.identity;
-            }
-            {
-                // Get mesh
-                if (prefab.TryGetComponent<UnityEngine.MeshFilter>(out var filter) == true) {
-                    ent.Get<MeshFilterComponent>().mesh = new ME.BECS.Addons.RuntimeObjectReference<UnityEngine.Mesh>(filter.sharedMesh, world.id);
+            var queue = new System.Collections.Generic.Queue<TransformItem>();
+            queue.Enqueue(new TransformItem() {
+                obj = prefab,
+                parent = parentEnt,
+            });
+            Ent result = default;
+            while (queue.Count > 0) {
+
+                var item = queue.Dequeue();
+                var obj = item.obj;
+                var parent = item.parent;
+                var ent = Ent.New(world);
+                if (result.IsAlive() == false) result = ent;
+                var tr = ent.GetOrCreateAspect<TransformAspect>();
+                ent.SetParent(parent);
+                tr.Set(obj);
+                if (parent.IsAlive() == true) {
+                    tr.worldMatrix = math.mul(obj.localToWorldMatrix, obj.root.localToWorldMatrix.inverse);
+                } else {
+                    tr.worldMatrix = float4x4.identity;
                 }
 
-                // Get rendering
-                if (prefab.TryGetComponent<UnityEngine.MeshRenderer>(out var renderer) == true) {
-                    ref var ren = ref ent.Get<MeshRendererComponent>();
-                    ren.material = new ME.BECS.Addons.RuntimeObjectReference<UnityEngine.Material>(renderer.sharedMaterial, world.id);
-                    ren.shadowCastingMode = renderer.shadowCastingMode;
-                    ren.receiveShadows = renderer.receiveShadows;
-                    ren.layer = renderer.gameObject.layer;
-                    ren.renderingLayerMask = renderer.renderingLayerMask;
-                    ren.rendererPriority = renderer.rendererPriority;
-                    ren.instanceID = renderer.GetInstanceID();
+                {
+                    // Get mesh
+                    if (obj.TryGetComponent<UnityEngine.MeshFilter>(out var filter) == true) {
+                        ent.Get<MeshFilterComponent>().mesh = new ME.BECS.Addons.RuntimeObjectReference<UnityEngine.Mesh>(filter.sharedMesh, world.id);
+                    }
+
+                    // Get rendering
+                    if (obj.TryGetComponent<UnityEngine.MeshRenderer>(out var renderer) == true) {
+                        ref var ren = ref ent.Get<MeshRendererComponent>();
+                        ren.material = new ME.BECS.Addons.RuntimeObjectReference<UnityEngine.Material>(renderer.sharedMaterial, world.id);
+                        ren.shadowCastingMode = renderer.shadowCastingMode;
+                        ren.receiveShadows = renderer.receiveShadows;
+                        ren.layer = renderer.gameObject.layer;
+                        ren.renderingLayerMask = renderer.renderingLayerMask;
+                        ren.rendererPriority = renderer.rendererPriority;
+                        ren.instanceID = renderer.GetInstanceID();
+                    }
+
+                    for (int i = 0; i < obj.transform.childCount; ++i) {
+                        var child = obj.transform.GetChild(i);
+                        queue.Enqueue(new TransformItem() {
+                            obj = child,
+                            parent = ent,
+                        });
+                    }
                 }
 
-                for (int i = 0; i < prefab.transform.childCount; ++i) {
-                    var child = prefab.transform.GetChild(i);
-                    ConstructEntFromPrefab(child, ent, in world);
-                }
             }
-            return ent;
+
+            return result;
 
         }
 
