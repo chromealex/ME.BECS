83febf8442861168832a6e85296cccaf1b6296fb
HDHero <leodenx@gmail.com>
2025-04-05 14:37:36 +0200

    [Commands] Added CommandAttackCleanSystem

---
+++ Addons/Prototype/Commands/Runtime/Systems/CommandAttackCleanSystem.cs
@@ -0,0 +1,36 @@
+﻿namespace ME.BECS.Commands {
+
+    using BURST = Unity.Burst.BurstCompileAttribute;
+    using Jobs;
+    using Pathfinding;
+    using Units;
+    
+    [BURST(CompileSynchronously = true)]
+    [RequiredDependencies(typeof(BuildGraphSystem))]
+    public struct CommandAttackCleanSystem : IUpdate {
+
+        [BURST(CompileSynchronously = true)]
+        public struct RemoveJob : IJobForComponents<UnitAttackCommandComponent> {
+
+
+            public void Execute(in JobInfo jobInfo, in Ent ent, ref UnitAttackCommandComponent attackCommand) {
+                
+                if (attackCommand.target.IsAlive() == true) return;
+                ent.Remove<UnitAttackCommandComponent>();
+                var unitAspect = ent.GetAspect<UnitAspect>();
+                unitAspect.RemoveFromCommandGroup();
+                unitAspect.IsHold = false;
+                
+            }
+
+        }
+
+        public void OnUpdate(ref SystemContext context) {
+            
+            var handle = context.Query().AsParallel().Schedule<RemoveJob, UnitAttackCommandComponent>();
+            context.SetDependency(handle);        
+        }
+
+    }
+
+}
