f066f79278b095ab6dad36764c439be81ccf019b
Alex Silaev <chrome.alex@gmail.com>
2025-04-27 22:03:42 +0300

    [Core] Math extensions using fix

--- Runtime/Extensions/MathExtensions.cs
+++ Runtime/Extensions/MathExtensions.cs
@@ -2,10 +2,12 @@
 using tfloat = sfloat;
 using ME.BECS.FixedPoint;
 using Bounds = ME.BECS.FixedPoint.AABB;
+using Rect = ME.BECS.FixedPoint.Rect;
 #else
 using tfloat = System.Single;
 using Unity.Mathematics;
 using Bounds = UnityEngine.Bounds;
+using Rect = UnityEngine.Rect;
 #endif
 
 namespace ME.BECS {
