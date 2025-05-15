87bfb28be9a8f431595d6c548f5d2ca3c33bff10
HDHero <leodenx@gmail.com>
2025-05-15 16:18:58 +0200

    [FogOfWar] Finished UpdateTextureSystemGPU

--- Addons/FogOfWar/Runtime/Systems/Visual/UpdateTextureSystemGPU.cs
+++ Addons/FogOfWar/Runtime/Systems/Visual/UpdateTextureSystemGPU.cs
@@ -1,17 +1,32 @@
+#if FIXED_POINT
+using tfloat = sfloat;
+using ME.BECS.FixedPoint;
+#else
+using tfloat = System.Single;
+using Unity.Mathematics;
+#endif
 using Unity.Collections.LowLevel.Unsafe;
 
 namespace ME.BECS.FogOfWar {
-    
+
     using BURST = Unity.Burst.BurstCompileAttribute;
-    using Unity.Jobs;
     using ME.BECS.Players;
     using Unity.Collections;
-    using ME.BECS.Jobs;
-    using static Cuts;
 
-    //[BURST(CompileSynchronously = true)]
     [RequiredDependencies(typeof(CreateTextureSystem))]
-    public unsafe struct UpdateTextureSystemGPU : IAwake, IUpdate {
+    public unsafe struct UpdateTextureSystemGPU : IAwake, IUpdate, IDestroy {
+
+        private const int THREAD_GROUPS = 4;
+
+        private static readonly int texSp = UnityEngine.Shader.PropertyToID("_Tex");
+        private static readonly int widthSp = UnityEngine.Shader.PropertyToID("_Width");
+        private static readonly int outSpeedSp = UnityEngine.Shader.PropertyToID("_FadeOutSpeed");
+        private static readonly int inSpeedSp = UnityEngine.Shader.PropertyToID("_FadeInSpeed");
+        private static readonly int bytesPerNodeSp = UnityEngine.Shader.PropertyToID("_BytesPerNode");
+        private static readonly int nodesSp = UnityEngine.Shader.PropertyToID("_Nodes");
+        private static readonly int exploredSp = UnityEngine.Shader.PropertyToID("_Explored");
+        private static readonly int deltaTimeSp = UnityEngine.Shader.PropertyToID("_DeltaTime");
+        private static readonly int fadeSp = UnityEngine.Shader.PropertyToID("_UseFade");
 
         public sfloat fadeInSpeed;
         public sfloat fadeOutSpeed;
@@ -19,191 +34,107 @@ namespace ME.BECS.FogOfWar {
         private ClassPtr<UnityEngine.ComputeBuffer> nodesBuffer;
         private ClassPtr<UnityEngine.ComputeBuffer> exploredBuffer;
         private ClassPtr<UnityEngine.RenderTexture> renderTexture;
-        
+
         private Ent lastActivePlayer;
         private ulong lastTick;
-        
+        private int intsCount;
 
-        [BURST(CompileSynchronously = true, FloatMode = Unity.Burst.FloatMode.Fast, FloatPrecision = Unity.Burst.FloatPrecision.Low, OptimizeFor = Unity.Burst.OptimizeFor.Performance)]
-        public struct UpdateJob : IJobParallelFor {
-
-            public sfloat dt;
-            public sfloat fadeInSpeed;
-            public sfloat fadeOutSpeed;
-            public FogOfWarStaticComponent props;
-            public FogOfWarComponent fow;
-            public uint textureWidth;
-            [NativeDisableParallelForRestriction]
-            [NativeDisableUnsafePtrRestriction]
-            public UnityEngine.Color32* currentBuffer;
-            public byte useFade;
-
-            public void Execute(int index) {
-
-                var w = this.textureWidth;
-                {
-                    var fowX = (uint)(index % w);
-                    var fowY = (uint)(index / w);
-                    ref var color = ref this.currentBuffer[index];
-                    if (this.useFade == 1) {
-                        if (FogOfWarUtils.IsVisible(in this.props, in this.fow, fowX, fowY) == true) {
-                            color.r = (byte)(color.r + (255 - color.r) * this.dt * this.fadeInSpeed);
-                        } else {
-                            color.r = (byte)(color.r + (0 - color.r) * this.dt * this.fadeOutSpeed);
-                        }
-
-                        if (FogOfWarUtils.IsExplored(in this.props, in this.fow, fowX, fowY) == true) {
-                            color.g = (byte)(color.g + (255 - color.g) * this.dt * this.fadeInSpeed);
-                        }
-                    } else {
-                        if (FogOfWarUtils.IsVisible(in this.props, in this.fow, fowX, fowY) == true) {
-                            color.r = 255;
-                        } else {
-                            color.r = 0;
-                        }
-
-                        if (FogOfWarUtils.IsExplored(in this.props, in this.fow, fowX, fowY) == true) {
-                            color.g = 255;
-                        }
-                    }
-                }
-                
-            }
-
-        }
-
-        [BURST(CompileSynchronously = true, FloatMode = Unity.Burst.FloatMode.Fast, FloatPrecision = Unity.Burst.FloatPrecision.Low, OptimizeFor = Unity.Burst.OptimizeFor.Performance)]
-        public struct UpdateTextureJob : IJob {
-
-            [NativeDisableParallelForRestriction]
-            [NativeDisableUnsafePtrRestriction]
-            public byte* currentBuffer;
-            public FogOfWarComponent fow;
-            
-            public void Execute() {
-
-                _memcpy(this.fow.nodes.GetUnsafePtr(), (safe_ptr)this.currentBuffer, this.fow.nodes.Length);
-
-            }
-
-        }
-
-        [BURST(CompileSynchronously = true, FloatMode = Unity.Burst.FloatMode.Fast, FloatPrecision = Unity.Burst.FloatPrecision.Low, OptimizeFor = Unity.Burst.OptimizeFor.Performance)]
-        public struct ClearTextureJob : IJob {
-
-            [NativeDisableParallelForRestriction]
-            [NativeDisableUnsafePtrRestriction]
-            public UnityEngine.Color32* currentBuffer;
-            public uint length;          
-            public void Execute() {
-                
-                FogOfWarUtils.CleanUpTexture(this.currentBuffer, this.length);
-                
-            }
-
-        }
-
-        public struct ApplyTextureJob : IJobMainThread {
-
-            public CreateTextureSystem system;
-            
-            public void Execute() {
-                
-                this.system.GetTexture().Apply(false);
-                
-            }
-
-        }
 
         public void OnAwake(ref SystemContext context) {
-            
-            var mapSize = context.world.parent.GetSystem<CreateSystem>().mapSize;
-            var resolution = context.world.parent.GetSystem<CreateSystem>().resolution;
-            var fowSize = ME.BECS.FixedPoint.math.max(32u, (ME.BECS.FixedPoint.uint2)(mapSize * resolution));
+
+            var fowSize = context.world.parent.GetSystem<CreateSystem>().GetHeights().Read<FogOfWarStaticComponent>().size;
+
             this.renderTexture = new ClassPtr<UnityEngine.RenderTexture>(new UnityEngine.RenderTexture((int)fowSize.x, (int)fowSize.y, 32) {
                 enableRandomWrite = true,
             });
 
-            this.nodesBuffer = new ClassPtr<UnityEngine.ComputeBuffer>(new UnityEngine.ComputeBuffer((int)(fowSize.x * fowSize.y * FogOfWarUtils.BYTES_PER_NODE), sizeof(byte), UnityEngine.ComputeBufferType.Default));
-            this.exploredBuffer = new ClassPtr<UnityEngine.ComputeBuffer>(new UnityEngine.ComputeBuffer((int)(fowSize.x * fowSize.y * FogOfWarUtils.BYTES_PER_NODE), sizeof(byte), UnityEngine.ComputeBufferType.Default));
-            this.shader.Value.SetTexture(0, "_Tex", this.renderTexture.Value);
-            this.shader.Value.SetTexture(1, "_Tex", this.renderTexture.Value);
-            this.shader.Value.SetBuffer(1, "_Nodes", this.nodesBuffer.Value);
-            this.shader.Value.SetBuffer(1, "_Explored", this.exploredBuffer.Value);
-            
-        }
-        
-        
-        public struct Job : IJob {
-            
-            public ObjectReference<UnityEngine.ComputeShader> shader;
-            public ME.BECS.FixedPoint.uint2 size;
-            
-            public void Execute() {
+            uint bytesCount = fowSize.x * fowSize.y * FogOfWarUtils.BYTES_PER_NODE;
+            this.intsCount = (int)math.ceil(bytesCount / 4f);
+            this.nodesBuffer = new ClassPtr<UnityEngine.ComputeBuffer>(new UnityEngine.ComputeBuffer(this.intsCount, sizeof(int), UnityEngine.ComputeBufferType.Default));
+            this.exploredBuffer = new ClassPtr<UnityEngine.ComputeBuffer>(new UnityEngine.ComputeBuffer(this.intsCount, sizeof(int), UnityEngine.ComputeBufferType.Default));
+            this.InitShader(fowSize);
 
-                int sizeX = Unity.Mathematics.math.ceilpow2(Unity.Mathematics.math.ceillog2(this.size.x));
-                int sizeY = Unity.Mathematics.math.ceilpow2(Unity.Mathematics.math.ceillog2(this.size.y));
-                // this.shader.Value.Dispatch(0, this.size.x / 8, this.size.y / 8, 1);
-                // this.shader.Value.Dispatch(1, this.size.x / 8, this.size.y / 8, 1);
-                
-            }
+        }
+
+        private void InitShader(uint2 fowSize) {
+
+            this.shader.Value.SetTexture(0, texSp, this.renderTexture.Value);
+            this.shader.Value.SetInt(widthSp, (int)fowSize.x);
+            this.shader.Value.SetFloat(outSpeedSp, (float)this.fadeOutSpeed);
+            this.shader.Value.SetFloat(inSpeedSp, (float)this.fadeInSpeed);
+            this.shader.Value.SetInt(bytesPerNodeSp, FogOfWarUtils.BYTES_PER_NODE);
+            this.shader.Value.SetTexture(1, texSp, this.renderTexture.Value);
+            this.shader.Value.SetBuffer(1, nodesSp, this.nodesBuffer.Value);
+            this.shader.Value.SetBuffer(1, exploredSp, this.exploredBuffer.Value);
+
+        }
+
+        private void UpdateShader(tfloat dt, bool useFade) {
+
+            this.shader.Value.SetFloat(deltaTimeSp, (float)dt);
+            this.shader.Value.SetFloat(fadeSp, useFade == true ? 1f : 0f);
 
         }
 
         public void OnUpdate(ref SystemContext context) {
 
+            {
+                //there is no need to init shader every frame, this is here cos of debug shader purposes
+                // var mapSize = context.world.parent.GetSystem<CreateSystem>().mapSize;
+                // var resolution = context.world.parent.GetSystem<CreateSystem>().resolution;
+                // var fowSize = math.max(32u, (uint2)(mapSize * resolution));
+                // this.InitShader(fowSize);
+            }
+
             var logicWorld = context.world.parent;
             E.IS_CREATED(logicWorld);
-            
-            var createTexture = context.world.GetSystem<CreateTextureSystem>();
-            
-            var buffer = createTexture.GetBuffer();
-            var bufferPtr = (UnityEngine.Color32*)buffer.GetUnsafePtr();
+
             var playersSystem = logicWorld.GetSystem<PlayersSystem>();
             var activePlayer = playersSystem.GetActivePlayer();
             var useFade = true;
             if (this.lastActivePlayer != activePlayer.ent) {
                 // clean up textures because we need to rebuild them for current player
-                context.SetDependency(new ClearTextureJob() {
-                    currentBuffer = bufferPtr,
-                    length = (uint)buffer.Length,
-                }.Schedule(context.dependsOn));
+                this.shader.Value.Dispatch(0, this.renderTexture.Value.width / THREAD_GROUPS, this.renderTexture.Value.height / THREAD_GROUPS, 1);
                 useFade = false;
             }
             this.lastActivePlayer = activePlayer.ent;
             var fow = activePlayer.readTeam.Read<FogOfWarComponent>();
-            
-            this.lastTick = logicWorld.CurrentTick;
-        
-            var system = logicWorld.GetSystem<CreateSystem>();
-            var props = system.heights.Read<FogOfWarStaticComponent>();
-            var handle = new UpdateJob() {
-                dt = context.deltaTime,
-                fadeInSpeed = this.fadeInSpeed,
-                fadeOutSpeed = this.fadeOutSpeed,
-                props = props,
-                fow = fow,
-                textureWidth = props.size.x,
-                useFade = (byte)(useFade == true ? 1 : 0),
-                currentBuffer = bufferPtr,
-            }.Schedule(buffer.Length / 4, JobUtils.GetScheduleBatchCount(buffer.Length), context.dependsOn);
-            
-            handle = new ApplyTextureJob() {
-                system = createTexture,
-            }.Schedule(handle);
-            
-            
-            var mapSize = context.world.parent.GetSystem<CreateSystem>().mapSize;
-            var resolution = context.world.parent.GetSystem<CreateSystem>().resolution;
-            var fowSize = ME.BECS.FixedPoint.math.max(32u, (ME.BECS.FixedPoint.uint2)(mapSize * resolution));
+            this.UpdateShader(context.deltaTime, useFade);
 
-            handle = new Job() {
-                shader = this.shader,
-                size = fowSize,
-            }.Schedule(handle);
-            
-            context.SetDependency(handle);
-            
+            {
+                #if ENABLE_UNITY_COLLECTIONS_CHECKS
+                var nodesSafety = CollectionHelper.CreateSafetyHandle(Constants.ALLOCATOR_TEMP);
+                #endif
+                var nodesInt = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<int>(fow.nodes.GetUnsafePtr().ptr, this.intsCount, Allocator.None);
+                #if ENABLE_UNITY_COLLECTIONS_CHECKS
+                NativeArrayUnsafeUtility.SetAtomicSafetyHandle(ref nodesInt, nodesSafety);
+                #endif
+                this.nodesBuffer.Value.SetData(nodesInt);
+            }
+
+            {
+                #if ENABLE_UNITY_COLLECTIONS_CHECKS
+                var expSafety = CollectionHelper.CreateSafetyHandle(Constants.ALLOCATOR_TEMP);
+                #endif
+                var expInt = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<int>(fow.explored.GetUnsafePtr().ptr, this.intsCount, Allocator.None);
+                #if ENABLE_UNITY_COLLECTIONS_CHECKS
+                NativeArrayUnsafeUtility.SetAtomicSafetyHandle(ref expInt, expSafety);
+                #endif
+                this.exploredBuffer.Value.SetData(expInt);
+            }
+
+            this.shader.Value.Dispatch(1, this.renderTexture.Value.width / THREAD_GROUPS, this.renderTexture.Value.height / THREAD_GROUPS, 1);
+            UnityEngine.Graphics.CopyTexture(this.renderTexture.Value, context.world.GetSystem<CreateTextureSystem>().GetTexture());
+            context.SetDependency(context.dependsOn);
+
+        }
+
+        public void OnDestroy(ref SystemContext context) {
+
+            this.nodesBuffer.Value.Release();
+            this.exploredBuffer.Value.Release();
+            this.renderTexture.Value.Release();
+
         }
 
     }
