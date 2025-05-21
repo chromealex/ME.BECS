c8a4d293e7f733b62be36012a7b9b9bfe5f3ecc0
HDHero <leodenx@gmail.com>
2025-05-21 13:22:47 +0200

    [Attack] Removed test job from MoveToAttackerSystem

--- Addons/Prototype/Attack/Runtime/Systems/MoveToAttackerSystem.cs
+++ Addons/Prototype/Attack/Runtime/Systems/MoveToAttackerSystem.cs
@@ -21,7 +21,7 @@ namespace ME.BECS.Attack {
     [BURST(CompileSynchronously = true)]
     [UnityEngine.Tooltip("Move unit if it was damaged and is not attacking and without hold")]
     [RequiredDependencies(typeof(BuildGraphSystem))]
-    public struct MoveToAttackerSystem : IUpdate, IStart {
+    public struct MoveToAttackerSystem : IUpdate {
 
         [BURST(CompileSynchronously = true)]
         public struct MoveToAttackerJob : IJobFor2Aspects1Components<UnitAspect, TransformAspect, DamageTookEvent> {
@@ -136,54 +136,6 @@ namespace ME.BECS.Attack {
 
         }
         
-        public static float3 test;
-        public Ent testTarget;
-        
-        public struct TestJob : IJobForAspects<UnitAspect> {
-
-            public BuildGraphSystem buildGraphSystem;
-            public Ent testTarget;
-
-            public void Execute(in JobInfo jobInfo, in Ent ent, ref UnitAspect unit) {
-                if (unit.readOwner.GetAspect<ME.BECS.Players.PlayerAspect>().readIndex != 1) return;
-                if (unit.readUnitCommandGroup.IsAlive() == true 
-                    && math.all(unit.readUnitCommandGroup.Read<CommandMove>().targetPosition == test)) return;
-                // CommandsUtils.SetCommand(in this.buildGraphSystem, in unit, new ME.BECS.Commands.CommandMove() {
-                //     targetPosition = test,
-                // }, jobInfo);
-
-                this.testTarget.GetAspect<TransformAspect>().position = test;
-                if (math.all(test == float3.zero)) return;
-                if (unit.readUnitCommandGroup.IsAlive() == true && unit.readUnitCommandGroup.Read<CommandAttack>().target == this.testTarget) return; 
-                CommandsUtils.SetCommand(in this.buildGraphSystem, in unit, new ME.BECS.Commands.CommandAttack() {
-                    target = this.testTarget,
-                }, jobInfo);
-                
-
-                // if (unit.readUnitCommandGroup.IsAlive() == false) {
-                //     var selectionGroupAspect = UnitUtils.CreateSelectionTempGroup(1, in jobInfo);
-                //     selectionGroupAspect.Add(in unit);
-                //     UnitUtils.CreateCommandGroup(this.buildGraphSystem.GetTargetsCapacity(), in selectionGroupAspect, in jobInfo);
-                //     selectionGroupAspect.Destroy();
-                // } 
-                //
-                // PathUtils.UpdateTarget(in this.buildGraphSystem, unit.readUnitCommandGroup.GetAspect<UnitCommandGroupAspect>(),  test, in jobInfo);
-                //
-            }
-
-        }
-
-
-        public void OnStart(ref SystemContext context) {
-            var nativeArray = context.Query().WithAspect<UnitAspect>().Without<IsUnitStaticComponent>().ToArray();
-            foreach (var ent in nativeArray) {
-                if (ent.GetAspect<UnitAspect>().readOwner.GetAspect<ME.BECS.Players.PlayerAspect>().readIndex != 1) {
-                    this.testTarget = ent;
-                    this.testTarget.GetAspect<UnitAspect>().IsHold = true;
-                    break;
-                }
-            }
-        }
 
         public void OnUpdate(ref SystemContext context) {
 
@@ -221,13 +173,6 @@ namespace ME.BECS.Attack {
                    .Schedule<ComebackAfterAttackJob, TransformAspect, UnitAspect, ComebackAfterAttackComponent>(new ComebackAfterAttackJob() {
                        buildGraphSystem = buildGraphSystem,
                    }).AddDependency(ref context);
-            
-            context.Query()
-                   .Without<IsUnitStaticComponent>()
-                   .Schedule<TestJob, UnitAspect>(new TestJob() {
-                       buildGraphSystem = buildGraphSystem,
-                       testTarget = this.testTarget,
-                   });
 
         }
 
