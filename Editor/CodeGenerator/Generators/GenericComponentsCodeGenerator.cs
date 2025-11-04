97db8210922b76904b49d2772d1fb8e70e968555
txcslm <hmmm.true@gmail.com>
2025-11-04 21:41:07 +0300

    Fix aot generating

--- Editor/CodeGenerator/Generators/GenericComponentsCodeGenerator.cs
+++ Editor/CodeGenerator/Generators/GenericComponentsCodeGenerator.cs
@@ -1,38 +1,44 @@
+using System;
 using System.Linq;
 using System.Reflection;
+using UnityEngine;
 
 namespace ME.BECS.Editor.Generators {
 
     public class GenericComponentsCodeGenerator : CustomCodeGenerator {
 
+        private static Type[] _cachedGenericComponentDefinitions;
+        private static readonly System.Collections.Generic.Dictionary<Type, Type[]> _cachedPossibleTypes = new System.Collections.Generic.Dictionary<Type, Type[]>();
+
         private struct GenericDefInfo {
-            public System.Type constraintType;
-            public System.Type[] possibleTypes;
+            public Type constraintType;
+            public Type[] possibleTypes;
         }
 
         private struct ValidationInfo {
             public string validationCall;
         }
 
-        public override void AddInitialization(System.Collections.Generic.List<string> dataList, System.Collections.Generic.List<System.Type> references) {
-            
-            var genericComponentDefinitions = UnityEditor.TypeCache.GetTypesDerivedFrom(typeof(IComponent))
+        public override void AddInitialization(System.Collections.Generic.List<string> dataList, System.Collections.Generic.List<Type> references) {
+            _cachedGenericComponentDefinitions ??= UnityEditor.TypeCache.GetTypesDerivedFrom(typeof(IComponent))
                 .Where(x => x.IsGenericTypeDefinition && x.IsValueType && typeof(IComponent).IsAssignableFrom(x))
                 .ToArray();
+            var genericComponentDefinitions = _cachedGenericComponentDefinitions;
 
-            var allInstantiations = new System.Collections.Generic.HashSet<System.Type>();
+            var allInstantiations = new System.Collections.Generic.HashSet<Type>();
+            var referencesSet = new System.Collections.Generic.HashSet<Type>(references);
 
-            this.cache.SetKey("GenericDefInfo");
+            cache.SetKey("GenericDefInfo");
             foreach (var genericDef in genericComponentDefinitions) {
                 
-                if (this.IsValidTypeForAssembly(genericDef, true) == false) continue;
+                if (IsValidTypeForAssembly(genericDef, true) == false) continue;
                 if (genericDef.GetGenericArguments().Length != 1) continue;
 
                 GenericDefInfo defInfo;
-                if (this.cache.TryGetValue<GenericDefInfo>(genericDef, out var cachedDefInfo) == false) {
+                if (cache.TryGetValue<GenericDefInfo>(genericDef, out var cachedDefInfo) == false) {
                     var typeParams = genericDef.GetGenericArguments();
-                    System.Type constraintType = null;
-                    System.Type[] allConstraints = null;
+                    Type constraintType = null;
+                    Type[] allConstraints = null;
                     if (typeParams.Length > 0) {
                         allConstraints = typeParams[0].GetGenericParameterConstraints();
                         constraintType = allConstraints.FirstOrDefault(x => x.IsInterface == true);
@@ -41,12 +47,15 @@ namespace ME.BECS.Editor.Generators {
                         }
                     }
 
-                    System.Type[] possibleTypes = null;
+                    Type[] possibleTypes = null;
                     if (constraintType != null) {
                         if (constraintType.IsInterface) {
-                            possibleTypes = UnityEditor.TypeCache.GetTypesDerivedFrom(constraintType)
-                                .Where(x => x.IsValueType && !x.IsGenericTypeDefinition && x.IsVisible)
-                                .ToArray();
+                            if (!_cachedPossibleTypes.TryGetValue(constraintType, out possibleTypes)) {
+                                possibleTypes = UnityEditor.TypeCache.GetTypesDerivedFrom(constraintType)
+                                    .Where(x => x.IsValueType && !x.IsGenericTypeDefinition && x.IsVisible)
+                                    .ToArray();
+                                _cachedPossibleTypes[constraintType] = possibleTypes;
+                            }
                         } else if (constraintType.IsValueType) {
                             possibleTypes = new[] { constraintType };
                         }
@@ -54,21 +63,20 @@ namespace ME.BECS.Editor.Generators {
 
                     defInfo = new GenericDefInfo {
                         constraintType = constraintType,
-                        possibleTypes = possibleTypes ?? new System.Type[0]
+                        possibleTypes = possibleTypes ?? Type.EmptyTypes
                     };
-                    this.cache.Add(genericDef, defInfo);
+                    cache.Add(genericDef, defInfo);
                 } else {
                     defInfo = cachedDefInfo;
                 }
 
                 if (defInfo.constraintType != null && defInfo.possibleTypes.Length > 0) {
                     foreach (var possibleType in defInfo.possibleTypes) {
-                        if (this.IsValidTypeForAssembly(possibleType) == false) continue;
+                        if (IsValidTypeForAssembly(possibleType) == false) continue;
                         try {
                             var instantiated = genericDef.MakeGenericType(possibleType);
-                            if (typeof(IComponent).IsAssignableFrom(instantiated)) {
-                                allInstantiations.Add(instantiated);
-                                if (references.Contains(instantiated) == false) {
+                            if (allInstantiations.Add(instantiated)) {
+                                if (referencesSet.Add(instantiated)) {
                                     references.Add(instantiated);
                                 }
                             }
@@ -78,22 +86,22 @@ namespace ME.BECS.Editor.Generators {
                 }
             }
 
-            var validatedTypes = new System.Collections.Generic.HashSet<System.Type>();
-            this.cache.SetKey("ValidationInfo");
+            var validatedTypes = new System.Collections.Generic.HashSet<Type>();
+            cache.SetKey("ValidationInfo");
             foreach (var componentType in allInstantiations.OrderBy(x => x.FullName)) {
                 if (!validatedTypes.Add(componentType)) continue;
 
                 ValidationInfo validationInfo;
-                if (this.cache.TryGetValue<ValidationInfo>(componentType, out var cachedValidation) == false) {
+                if (cache.TryGetValue<ValidationInfo>(componentType, out var cachedValidation) == false) {
                     var componentTypeName = EditorUtils.GetDataTypeName(componentType);
                     var isTag = (System.Runtime.InteropServices.Marshal.SizeOf(componentType) <= 1 &&
-                                 componentType.GetFields(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic).Length == 0).ToString().ToLower();
+                                 componentType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).Length == 0).ToString().ToLower();
 
                     string validationCall;
                     if (typeof(IConfigComponentStatic).IsAssignableFrom(componentType)) {
                         validationCall = $"StaticTypes<{componentTypeName}>.ValidateStatic(isTag: {isTag});";
                     } else if (typeof(IComponentShared).IsAssignableFrom(componentType)) {
-                        var hasCustomHash = componentType.GetMethod(nameof(IComponentShared.GetHash), System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic) != null ||
+                        var hasCustomHash = componentType.GetMethod(nameof(IComponentShared.GetHash), BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) != null ||
                                             componentType.GetInterfaceMap(typeof(IComponentShared)).TargetMethods.Any(m => m.IsPrivate == true && m.Name == typeof(IComponentShared).FullName + "." + nameof(IComponentShared.GetHash));
                         validationCall = $"StaticTypes<{componentTypeName}>.ValidateShared(isTag: {isTag}, hasCustomHash: {hasCustomHash.ToString().ToLower()});";
                     } else {
@@ -103,7 +111,7 @@ namespace ME.BECS.Editor.Generators {
                     validationInfo = new ValidationInfo {
                         validationCall = validationCall
                     };
-                    this.cache.Add(componentType, validationInfo);
+                    cache.Add(componentType, validationInfo);
                 } else {
                     validationInfo = cachedValidation;
                 }
