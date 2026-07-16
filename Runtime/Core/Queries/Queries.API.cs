8f9572258b769bcbec91b5c6c3fd8b6948f0cf0c
Alex Silaev <chrome.alex@gmail.com>
2026-07-16 10:09:36 +0300

    [API] Query withInactive flag added

--- Runtime/Core/Queries/Queries.API.cs
+++ Runtime/Core/Queries/Queries.API.cs
@@ -31,20 +31,20 @@ namespace ME.BECS {
     [IgnoreProfiler]
     public static class APIExt {
 
-        public static QueryBuilder Query(this in SystemContext context) {
-            return API.Query(in context);
+        public static QueryBuilder Query(this in SystemContext context, bool withInactive = false) {
+            return API.Query(in context, withInactive);
         }
 
-        public static QueryBuilder Query(this in SystemContext context, JobHandle dependsOn) {
-            return API.Query(in context, dependsOn);
+        public static QueryBuilder Query(this in SystemContext context, JobHandle dependsOn, bool withInactive = false) {
+            return API.Query(in context, dependsOn, withInactive);
         }
 
-        public static QueryBuilder Query<T>(this T system, in SystemContext context) where T : unmanaged, ISystem {
-            return API.Query(in context);
+        public static QueryBuilder Query<T>(this T system, in SystemContext context, bool withInactive = false) where T : unmanaged, ISystem {
+            return API.Query(in context, withInactive);
         }
 
-        public static QueryBuilder Query<T>(this T system, in SystemContext context, JobHandle dependsOn) where T : unmanaged, ISystem {
-            return API.Query(in context, dependsOn);
+        public static QueryBuilder Query<T>(this T system, in SystemContext context, JobHandle dependsOn, bool withInactive = false) where T : unmanaged, ISystem {
+            return API.Query(in context, dependsOn, withInactive);
         }
 
     }
@@ -52,20 +52,20 @@ namespace ME.BECS {
     [IgnoreProfiler]
     public static class API {
 
-        public static QueryBuilder Query(in World world, JobHandle dependsOn = default) {
-            return API.Query(QueryContext.Create(in world), dependsOn);
+        public static QueryBuilder Query(in World world, JobHandle dependsOn = default, bool withInactive = false) {
+            return API.Query(QueryContext.Create(in world), dependsOn, withInactive);
         }
 
-        public static QueryBuilder Query(in SystemContext systemContext) {
-            return API.Query((QueryContext)systemContext, systemContext.dependsOn);
+        public static QueryBuilder Query(in SystemContext systemContext, bool withInactive = false) {
+            return API.Query((QueryContext)systemContext, systemContext.dependsOn, withInactive);
         }
 
-        public static QueryBuilder Query(in SystemContext systemContext, JobHandle dependsOn) {
-            return API.Query((QueryContext)systemContext, JobHandle.CombineDependencies(systemContext.dependsOn, dependsOn));
+        public static QueryBuilder Query(in SystemContext systemContext, JobHandle dependsOn, bool withInactive = false) {
+            return API.Query((QueryContext)systemContext, JobHandle.CombineDependencies(systemContext.dependsOn, dependsOn), withInactive);
         }
 
         [IgnoreProfiler]
-        public static QueryBuilder Query(in QueryContext queryContext, JobHandle dependsOn = default) {
+        public static QueryBuilder Query(in QueryContext queryContext, JobHandle dependsOn = default, bool withInactive = false) {
 
             var allocator = WorldsTempAllocator.allocatorTemp.Get(queryContext.worldId).Allocator.ToAllocator;
             var builder = new QueryBuilder {
@@ -80,7 +80,7 @@ namespace ME.BECS {
                 scheduleMode = Unity.Jobs.LowLevel.Unsafe.ScheduleMode.Single,
                 builderDependsOn = dependsOn,
             };
-            builder.Without<IsInactive>();
+            if (withInactive == false) builder.Without<IsInactive>();
             
             return builder;
             
